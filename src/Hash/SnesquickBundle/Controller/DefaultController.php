90805906ed19d306532749ab870fdf34ebf4e4af
harraps <harraps3.0@gmail.com>
2015-04-17 14:42:43 +0200

    Init Symfony project
    Base for the symfony project YEAH !

---
+++ src/Hash/SnesquickBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hash\SnesquickBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('HashSnesquickBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Hash/SnesquickBundle/HashSnesquickBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hash\SnesquickBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HashSnesquickBundle extends Bundle
+{
+}
